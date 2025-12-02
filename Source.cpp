d5fcd657901e7064471823aa1cc83ddb7fde2a5a
Josu19 <64388373+Josu1906@users.noreply.github.com>
2025-12-02 14:58:37 +0100

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,112 +1,141 @@
 #include <iostream>
-using namespace std;
+#include <string>
 #include <cmath>
 
-void fhorizontal(int i);
+using namespace std;
 
-void lecturaConLimites(int a, int b);
+const int N = 3;
+typedef int tVector[N];
+
+typedef struct {
+	tVector vector;
+	int contador;
+} tV;
+
+typedef enum {suma, resta, modulo, producto, normalizacion, obtencion} tOperaciones;
+
+void fInitArrays(tV vector);
+
+void fOperacion(tV v1, tV v2, tV vR);
+
+void fEx(int op, tV v1, tV v2, tV vR);
 
 int main() {
 
-	/*int num, i;
-	cout << "Dime el número que quieras transformar: " << endl;
-	cin >> num;
-	for (int i = 1; i <= num; i++) {
+	int dim = 0;
 
-		fhorizontal(i);
+	tV vector1;
+	tV vector2;
+	tV vectorR;
 
-	}*/
+	cout << "Dimension de los dos vectores (1 al 3): ";
+	cin >> dim;
 
-	//int sum = 0, modulo, x;
-
-
-	//for (int i = 100; i < 1000; i++) {
-	//	modulo = i;
-	//	for (int j = 1; j <= 3; j++) {
-
-	//		x = modulo % 10;
-	//		modulo = modulo / 10;
-
-	//		/*cout << x << ", " << modulo << endl;*/
+	vector1.contador = dim;
+	vector2.contador = vector1.contador;
 
 
-	//		sum += x * x * x;
+	fInitArrays(vector1);
+	fInitArrays(vector2);
+
+	fOperacion(vector1, vector2, vectorR);
 
 
-	//		
-	//		if (sum == i) {
-	//			cout << i << " " << "cumple la condicion" << endl;
-	//		}
-	//	}
-
-	//	sum = 0;
-	//}
-
-	/*int x = 0;
-	lecturaConLimites(10, 20);*/
-
-	double x = 0.0;
-
-	cout << "Da un numero decimal: " << endl;
-	cin >> x;
-
-	x = x * 100;
-	x = round(x);
-	x = x / 100;
-
-	cout << "El numero redondeado a las centesimas es " << x;
-
+	return 0;
 
 }
 
-void lecturaConLimites(int a, int b) {
+void fInitArrays(tV vector) {
+	int dim = vector.contador;
+	cout << "Agrega los valores para el vector";
 
-	bool c = false;
-
-	int x = 0;
-	cout << "Da un valor cualquiera que este dentro del intervalo: " << endl;
-	cin >> x;
-
-	while (!c) {
-
-
-		if (a <= x && x <= b) {
-			cout << "Esta dentro del intervalo" << endl;
-			c = true;
-		}
-		else {
-			if (x < a) {
-				cout << "Es un numero mayor" << endl;
-			}
-			else if (x > b){
-				cout << "Es un numero menor" << endl;
-			}
-			cout << "Intenalo de nuevo: " << endl;
-			cin >> x;
-		}
-
-		
+	for (int i = 0; i < (dim - 1); i++) {
+		cin >> vector.vector[i];
 	}
+}
 
+void fOperacion(tV v1, tV tv2, tV tvR) {
+	cout << "1. Suma de vectores" << endl;
+	cout << "2. Resta de vectores" << endl;
+	cout << "3. Modulo de vectores" << endl;
+	cout << "4. Producto de vectores de vectores" << endl;
+	cout << "5. Normalizacion de vectores" << endl;
+	cout << "6. Obtencion del valor maximo de los vectores" << endl;
+
+
+	int op = 0;
+
+	cin >> op;
 
 }
 
-void fhorizontal(int i) {
+void fEx(int op, tV v1, tV v2, tV vR) {
 
 	int suma = 0;
+	int modulo = 0;
+	int n_mayor = 0;
 
-	for (int j = 1; j <= 5; j++) {
+	switch (op) {
+		case 1:
+			for (int i = 0; i < v1.contador; i++) {
+				vR.vector[i] = v1.vector[i] + v2.vector[i];
+			}
+		break;
+		case 2:
+			for (int i = 0; i < v1.contador; i++) {
+				vR.vector[i] = v1.vector[i] - v2.vector[i];
+			}
+		break;
+		case 3:
+			for (int i = 0; i < v1.contador; i++) {
+				vR.vector[i] = v1.vector[i] - v2.vector[i];
+				vR.vector[i] = pow(vR.vector[i], 2);
+			}
 
-		if (j == 5) {
-			cout << suma << endl;
-		}
-		
-		else {
-			cout << i *  j << " ";
-			suma += i*j ;
-		}
+			for (int i = 0; i < v1.contador; i++) {
+					suma += vR.vector[i];
+			}
+
+			modulo = sqrt(suma);
+
+			break;
+		case 4:
+			for (int i = 0; i < v1.contador; i++) {
+				vR.vector[i] = v1.vector[i] - v2.vector[i];
+				vR.vector[i] = pow(vR.vector[i], 2);
+			}
+
+				for (int i = 0; i < v1.contador; i++) {
+					vR.vector[i] = v1.vector[i] * v2.vector[i];
+					suma += vR.vector[i];
+				}
+
+				modulo = sqrt(suma);
+
+			break;
+		case 5:
+			for (int i = 0; i < v1.contador; i++) {
+				vR.vector[i] = pow(v1.vector[i], 2);
+			}
+
+				for (int i = 0; i < v1.contador; i++) {
+					suma += vR.vector[i];
+				}
+
+				modulo = sqrt(suma);
+
+				for (int i = 0; i < v1.contador; i++) {
+					v1.vector[i] = v1.vector[i] / modulo;
+				}
+				break;
+			case 6:
+				for (int i = 0; i < v1.contador; i++) {
+					if (v1.vector[i] > n_mayor) {
+						n_mayor = v1.vector[i];
+					}
+				}
+			break;
+
 	}
 
 }
-
- 
